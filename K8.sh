d5a0716d9c156a3a840a61502685db908221a251
praveenenzoo <praveen.kumar1202@gmail.com>
2019-03-20 15:53:48 +0530

    Auto Install Kubernetes and Docker With flannel networking
    this script will install Kubernetes and Docker automatically .  all configuration will be done auto run; sysctl.conf file enty bridge networking, firewalld off ,etc.

---
+++ K8.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+sudo bash -c 'cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF'
+sudo systemctl disable firewalld && sudo systemctl stop firewalld
+sudo setenforce 0
+sudo bash -c 'echo "SELINUX=disabled" > /etc/sysconfig/selinux'
+sudo swapoff -a
+sudo sed -e '/swap/ s/^#*/#/' -i /etc/fstab
+sudo modprobe br_netfilter
+sudo echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install -y docker-ce
+sudo yum install -y kubelet kubeadm kubectl docker
+sudo systemctl enable kubelet && sudo systemctl start kubelet
+sudo systemctl enable docker && sudo systemctl start docker
+sudo sed -i 's/cgroup-driver=systemd/cgroup-driver=cgroupfs/g' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+systemctl daemon-reload
+systemctl restart kubelet
+sudo bash -c 'cat <<EOF >>  /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF'
+sudo sysctl --system
+sudo kubeadm init --apiserver-advertise-address=10.0.15.10 --pod-network-cidr=172.16.0.0/16
+rm -rf $HOME/.kube
+sudo mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+export kubever=$(kubectl version | base64 | tr -d '\n')
+sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
